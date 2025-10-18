e1ddbc3e5aa634bf0a6cb9b5c17bcb93f85bc635
Shaznay Walker <shaznay.walker@mymona.uwi.edu>
2025-10-17 21:45:48 -0500

    Implemented Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,4 @@
+//Exercise 1 
 window.addEventListener('DOMContentLoaded', () => {
     const board = document.getElementById('board');
     const squares = board.getElementsByTagName('div');
@@ -6,3 +7,24 @@ window.addEventListener('DOMContentLoaded', () => {
         square.classList.add('square');
     }
 });
+
+//Exercise 2
+window.addEventListener('DOMContentLoaded', () => {
+    const board = document.getElementById('board');
+    const squares = board.querySelectorAll('div');
+    let activePlayer = 'X';
+
+    squares.forEach(square => {
+        square.classList.add('square');
+
+        square.addEventListener('click', () => {
+            if (!square.textContent) {
+                square.textContent = activePlayer;
+                square.classList.add(activePlayer);
+                activePlayer = activePlayer === 'X' ? 'O': 'X';
+            }
+        });
+    });
+});
+
+
